f1c79ebf8aaa36afb547ce2e5802463c8bed8bc0
Ryoma Kai <706834+legnoh@users.noreply.github.com>
2023-07-25 23:10:36 +0900

    hotfix of 115 problem

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os,platform,time
+import requests
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromiumService
@@ -27,12 +28,20 @@ if __name__ == '__main__':
     print("create all metrics instances...")
     m = Info('aphorism', '格言をランダムに表示', registry=registry)
 
+    ch_version = requests.get("https://chromedriver.storage.googleapis.com/LATEST_RELEASE").text
+    if platform.system() == 'Linux':
+        ch_type = ChromeType.CHROMIUM
+    else:
+        ch_type = ChromeType.GOOGLE
+    ch_driver_manager = ChromeDriverManager(driver_version=ch_version, chrome_type=ch_type)
+    if platform.system() == 'Linux':
+        ch_service = ChromiumService(ch_driver_manager.install())
+    else:
+        ch_service = ChromeService(ch_driver_manager.install())
+
     while True:
 
-        if platform.system() == 'Linux':
-            driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
-        else:
-            driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+        driver = webdriver.Chrome(service=ch_service)
 
         print("get aphorism...")
         driver.get("https://dictionary.goo.ne.jp/quote/")
